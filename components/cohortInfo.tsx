ba17a3c720a34a3b56aad503a4f7150863064353
Utkarsh <78083867+hsraktu17@users.noreply.github.com>
2024-11-15 01:19:47 +0530

    join the cohort

--- components/cohortInfo.tsx
+++ components/cohortInfo.tsx
@@ -9,9 +9,12 @@ import arrowRight from "@/public/Arrow Right.svg";
 import Image from "next/image";
 export default function CohortInfo() {
   return (
-    <div className="flex items-center justify-center bg-[#00000066] h-[692px] cohort-main px-[120px] cohort-info" id="cohortInfo">
+    <div
+      className="flex items-center justify-center bg-[#00000066] h-[692px] cohort-main px-[120px] cohort-info"
+      id="cohortInfo"
+    >
       {/* Centering container */}
-      <div className="flex w-[1488px] min-h-[452px] gap-[96px] cohort-container">
+      <div className="flex w-[1488px] min-h-[452px] gap-[25px] cohort-container">
         {/* Left Box */}
         <div className="w-[724px] flex flex-col justify-center">
           <h2 className="text-[36px] font-bold text-white">
@@ -38,27 +41,29 @@ export default function CohortInfo() {
                 </span>
               </div>
             </div>
-            <div className="text-white w-[440px] cohort-accordion" >
+            <div className="text-white w-[440px] cohort-accordion">
               <Accordion type="single" collapsible>
                 <AccordionItem value="item-1" className="border-none">
                   <AccordionTrigger className="flex justify-between items-center text-left">
                     <div className="flex items-center">
                       <Image src={infoIcon} alt="Info" className="h-10" />
-                      <span className="ml-2 text-[18px]">Price increase from 15 Nov 11:59 PM</span>
+                      <span className="ml-2 text-[18px]">
+                        Price increase from 15 Nov 11:59 PM
+                      </span>
                     </div>
-                 {/* Replace with your arrow icon */}
+                    {/* Replace with your arrow icon */}
                   </AccordionTrigger>
                   <AccordionContent className="text-left accordion-content flex justify-between text-xl font-bold">
-                  <span>Until Nov 20</span> <span>INR 25000</span>
+                    <span>Until Nov 20</span> <span>INR 25000</span>
                   </AccordionContent>
                   <AccordionContent className="text-left accordion-content flex justify-between text-xl font-bold">
-                  <span>Until Nov 30</span> <span>INR 30000</span>
+                    <span>Until Nov 30</span> <span>INR 30000</span>
                   </AccordionContent>
                   <AccordionContent className="text-left accordion-content flex justify-between text-xl font-bold">
-                  <span>Until Dec 15</span> <span>INR 50000</span>
+                    <span>Until Dec 15</span> <span>INR 50000</span>
                   </AccordionContent>
                   <AccordionContent className="text-left accordion-content flex justify-between text-xl font-bold">
-                  <span>Until Dec 30</span> <span>INR 75000 </span>
+                    <span>Until Dec 30</span> <span>INR 75000 </span>
                   </AccordionContent>
                 </AccordionItem>
               </Accordion>
@@ -66,11 +71,15 @@ export default function CohortInfo() {
           </div>
 
           <div className="w-full h-auto p-8 flex flex-col gap-4 bg-[#00000033]">
-          <a href="https://pdhschool.notion.site/13de8b9ff79880cb8019e0da17497c4c?pvs=105" target="_blank" rel="noopener noreferrer">
-            <button className="bg-white w-full py-4 px-4 text-black text-[19px] font-bold hover:bg-gray-200 flex justify-between items-center">
-              Be an Early Bird – Apply Now
-              <Image src={arrowRight} alt="Right" />
-            </button>
+            <a
+              href="https://pdhschool.notion.site/13de8b9ff79880cb8019e0da17497c4c?pvs=105"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="bg-white w-full py-4 px-4 text-black text-[19px] font-bold hover:bg-gray-200 flex justify-between items-center">
+                Be an Early Bird – Apply Now
+                <Image src={arrowRight} alt="Right" />
+              </button>
             </a>
             <div className="text-white text-xs">
               Only 30 seats available – don’t miss out!
