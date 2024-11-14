0efba099c92a75e621db0f055ff1da84f8967de2
Utkarsh <78083867+hsraktu17@users.noreply.github.com>
2024-11-14 17:02:21 +0530

    viewport added

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,8 +54,6 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width"
           initial-scale="1.0"
-          maximum-scale="1.0"
-          user-scalable="no"
         />
         <meta name="theme-color" content="black" />
       </head>
